52bcf2e2b0e2e0b582997968ccd4547ed81a0db8
Suhaimee Yakoh <suhaime.yk@gmail.com>
2023-03-24 23:13:41 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ProjecLatex---Backend
+SELECT DATE_FORMAT(data_date, '%d/%m/%Y') AS formatted_date , data_pricetotal 
+FROM db_data;
+
--- test.js
+++ test.js
@@ -1,16 +1,18 @@
-app.post('/editreveal', jsonParser, function (req, res, next) {
 
-    connection.execute(
-        'INSERT INTO db_reveal_detail(reveal_id,reveal_sumtotal,reveal_pay,reveal_total)  VALUES(  ? , ? ,  ? , ? )',
-        [ req.body.reveal_id , req.body.reveal_sumtotal  , req.body.reveal_pay , req.body.reveal_total] ,
+app.get('/revealdisplayALL', jsonParser, function (req, res, next) {
+
+    connection.query(
+        'SELECT m.db_reveal_date, m.reveal_total as ผลรวมค่าปุ๋ย, c.customer_name as ชื่อ, u.users_name as ชื่อผู้ดูแล , md.reveal_sumtotal as ค่าหลังจ่าย , md.reveal_pay as ค่าที่จ่าย FROM db_reveal as m INNER JOIN db_customer as c ON m.users_id = c.customer_id INNER JOIN db_users as u ON u.users_id =  c.db_users_id INNER JOIN db_reveal_detail as md ON md.reveal_id =  m.reveal_id GROUP BY m.db_reveal_date, m.reveal_total, c.customer_name, u.users_name, md.reveal_sumtotal , md.reveal_pay ORDER BY m.db_reveal_date DESC;',
         function (err, results, fields) {
+            let status = "Ok";
             if (err) {
                 res.json({ status: 'error', message: err })
                 return
             }
-            res.json({ status: 'Ok' })
+            
+            // res.json({ results })
+            return res.send({ error: false ,status: status, data: results })
+            
         }
-
     );
-
 })
